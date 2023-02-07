e31d2cd2e4990e1df1f75b733df4ef1b729f67bd
jose sanchez <jose.sanchez.artist@gmail.com>
2023-02-07 13:57:38 -0800

    initial commit

---
+++ Api_Query_work/Program.cs
@@ -0,0 +1,13 @@
+﻿using Api_Query_work;
+using RestSharp;
+
+
+
+var key = new ApiKey();
+var client = new RestClient("https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5");
+var request = new RestRequest();
+request.AddHeader("X-RapidAPI-Key", key.ApiKey());
+request.AddHeader("X-RapidAPI-Host", "spotify23.p.rapidapi.com");
+var response = client.Execute(request).Content;
+
+Console.WriteLine(response);
