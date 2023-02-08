07653024de663051207509d04097de1c1d93acd7
jose sanchez <jose.sanchez.artist@gmail.com>
2023-02-07 16:48:28 -0800

    Initial Commit

---
+++ Api_Query_work/Chuck_Root.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_Query_work
+{
+    public class Chuck_Root
+    {
+
+    
+        public List<object> categories { get; set; }
+        public string created_at { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+
+
+}
+
--- Api_Query_work/Program.cs
+++ Api_Query_work/Program.cs
@@ -1,13 +1,27 @@
 ﻿using Api_Query_work;
+using Newtonsoft.Json;
 using RestSharp;
-
-
+using System.Collections.Generic;
 
 var key = new ApiKey();
-var client = new RestClient("https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5");
+
+var client = new RestClient("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random");
 var request = new RestRequest();
-request.AddHeader("X-RapidAPI-Key", key.ApiKey());
-request.AddHeader("X-RapidAPI-Host", "spotify23.p.rapidapi.com");
+request.AddHeader("accept", "application/json");
+request.AddHeader("X-RapidAPI-Key", key.Api());
 var response = client.Execute(request).Content;
 
-Console.WriteLine(response);
+var root = JsonConvert.DeserializeObject<List<Chuck_Root>>(response);
+
+foreach(var item in root) 
+{
+    Console.WriteLine(item.id);
+    Console.WriteLine(item.created_at);
+    Console.WriteLine(item.updated_at);
+    Console.WriteLine(item.url);
+    Console.WriteLine(item.value);
+    Console.WriteLine(item.icon_url);
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("=============================");
+}
